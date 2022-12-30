7e9fcd8c4f74cc5f9ab0100b71c00e1b0a397615
Nickita Danilovich <nickitadanilovich@gmail.com>
2022-12-29 20:46:56 -0500

    commit tree 96fa9da18c79099208cc4331d0b61a316cc0eecd

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const cot = [...cats, name];
+    return cot;
+}
+function prependCat(name){
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+function removeLastCat(){
+    const copy = cats.slice(0, cats.length-1);
+    return copy;
+}
+function removeFirstCat(){
+   const cat = cats.slice(1);
+   return cat;
+}
+
+
+
+
+
