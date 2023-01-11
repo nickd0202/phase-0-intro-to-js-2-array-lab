9caae658812d21280e70d95c21c41af5875690ea
Nickita Danilovich <nickitadanilovich@gmail.com>
2023-01-11 13:04:39 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const cat = [...cats];
+    cat.push(name);
+    return cat;
+}
+
+function prependCat(name){
+    let cat = [...cats];
+    cat.unshift(name);
+    return cat;
+}
+
+function removeLastCat(){
+    const cat = [...cats];
+    cat.pop();
+    return cat;
+}
+
+function removeFirstCat(){
+    const cat = [...cats];
+    cat.shift();
+    return cat;
+}
